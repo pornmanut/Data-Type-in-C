5551b3a46754c4055b22f2cb7f3844656b60e448
pornmanut <pornmanut@gmail.com>
2017-09-25 19:55:43 +0700

    add newNode and freeNode

--- Linked-list-in-C.c
+++ Linked-list-in-C.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <malloc.h>
 
-int main()
-{
-  printf("Hello world");
+typedef struct Node {
+  char info;
+  struct Node *next;
+}TYPE_NODE;
+
+typedef struct Node *TYPE_NODEPTR;
+
+TYPE_NODEPTR NewNode(char Item){
+  TYPE_NODEPTR node;
+  node = (TYPE_NODEPTR) malloc(sizeof(TYPE_NODE));
+  node -> info = Item;
+  node -> next = NULL;
+  return(node);
+}
+
+void FreeNode(TYPE_NODEPTR node){
+  free(node);
+}
+
+int main(){
+  printf("%lu\n",sizeof(TYPE_NODE));
   return 0;
 }
